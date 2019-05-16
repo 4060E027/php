2434a4a68ab68591531a3d8b93286ddd3e2c1112
4060E027 <37572140+4060E027@users.noreply.github.com>
2019-05-16 16:26:38 +0800

    Add files via upload

---
+++ great_teacher/php/first.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD>
+    <TITLE>Who Are You ?</TITLE>
+</HEAD>
+<BODY>
+<?php
+   define("Dragon","Hello , Who Are You ?");
+   echo Dragon;
+   phpinfo ();
+?>
+</BODY>
+<HTML>
---
+++ great_teacher/php/no'2.php
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD>
+    <TITLE>Who Are You ?</TITLE>
+</HEAD>
+<BODY>
+<?php
+   include_once("tree.inc");
+?>
+</BODY>
+<HTML>
